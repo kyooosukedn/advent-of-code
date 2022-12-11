4e912b61e3127308c567fb81f5dac1788bbfb6ca
kyooosukedn <kyooosukedn@github.com>
2022-12-11 21:27:55 +0100

    Sadly i still cant solve it so im gonna come back..

---
+++ AdventOfCode2022/src/Day05.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day05 {
+    public static void main(String[] args) throws IOException {
+        // I can't solve it i will come back tough (soon)
+    }
+}
