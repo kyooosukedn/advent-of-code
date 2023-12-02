a65d14efe747eefee372d7d355695fab8cef7ee4
kyooosukedn <danielpurnomo50@gmail.com>
2023-12-02 22:16:46 +0100

    initial advent

---
+++ 2023/day1.js
@@ -0,0 +1,15 @@
+// 
+//
+const fs = require('fs');
+const filePath = 'firstDayInput.txt';
+
+fs.read(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.log("Error: " + err);
+    return;
+  }
+})
+
+function name(params) {
+
+}
