4410433f3f707210ace5e9f0e0976e066313344a
kyooosukedn <danielpurnomo50@gmail.com>
2023-12-03 00:27:20 +0100

    day 1

--- 2023/day1.js
+++ 2023/day1.js
@@ -1,15 +1,28 @@
 // 
 //
 const fs = require('fs');
-const filePath = 'firstDayInput.txt';
+const filePath = './firstDayInput.txt';
 
-fs.read(filePath, 'utf8', (err, data) => {
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.log("Error: " + err);
+    console.error("Error: ", err);
     return;
   }
-})
 
-function name(params) {
+  const lines = data.split("\n");
 
-}
+  let totalSum = 0;
+  lines.forEach(line => {
+    const numbers = line.match(/\d/g);
+    console.log(numbers);
+    if (numbers && numbers.length >= 2) {
+      const firstDigit = numbers[0];
+      const lastDigit = parseInt(numbers[numbers.length - 1]);
+      const calibrationValue = parseInt(`${firstDigit}${lastDigit}`);
+      totalSum += calibrationValue;
+    }
+  });
+
+  console.log(totalSum);
+});
+
