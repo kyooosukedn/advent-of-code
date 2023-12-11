d778306621b9bac0f6440fd1e86ccf5d3164320b
kyooosukedn <danielpurnomo50@gmail.com>
2023-12-11 19:31:42 +0100

    day 4 part 2

--- 2023/day4_scratchcards.js
+++ 2023/day4_scratchcards.js
@@ -1,6 +1,7 @@
+
 const fs = require("fs");
 
-const input = fs.readFileSync("./scratchcards.txt", { encoding: "utf8" });
+const cards = fs.readFileSync("./scratchcards.txt", { encoding: "utf8" }).split("\n").filter(x => x.length);
 
 
 function toDict(numbers) {
@@ -10,7 +11,7 @@ function toDict(numbers) {
   }, {});
 }
 
-
+/*
 const contents = input.split("\n").filter(x => x.length).map(x => {
   const values = x.split(":")[1];
   console.log("values", values);
@@ -33,11 +34,49 @@ const contents = input.split("\n").filter(x => x.length).map(x => {
       }
     }
   });
+  console.log(points);
 
   return acc + points;
 }, 0);
 
 console.log("Sum of Points:", contents);
 
+// Part 2 using Union Find Path Compression
+// A copy of matching numbers
+*/
 
+function getPoints(x, index) {
+  const values = x.split(":")[1];
+  const [winners, totalNums] = values.split("|");
+  const winningData = winners.split(" ").filter(x => x);
+  const wDict = toDict(winningData.map(x => parseInt(x)));
+  const numbers = totalNums.split(" ").map(x => parseInt(x.trim()));
 
+  let points = 0;
+  numbers.forEach(element => {
+    if (wDict[element]) {
+      points++;
+    }
+  });
+
+  return new Array(points).fill(index + 1).map((x, i) => {
+    return x + i;
+  });
+ }
+
+ const toProcess = new Array(cards.length).fill(0).map((_, i) => i +1);
+ const seen = {};
+ const count = {};
+
+ while (toProcess.length) {
+  const index = toProcess.pop();
+  count[index] = count[index] ? count[index] + 1 : 1;
+  const points = seen[index] ? seen[index] : getPoints(cards[index - 1], index);
+  seen[index] = points;
+
+  toProcess.push(...points);
+ }
+
+ console.log(count, Object.keys(count).reduce((acc, x ) => {
+  return acc + count[x];
+ }, 0));
